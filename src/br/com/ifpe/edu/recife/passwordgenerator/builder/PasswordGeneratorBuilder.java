33d3b1b6ac4bc1a5000eab018249fa734e4f07cd
Rennan Prysthon <rennan.prysthon@liferay.com>
2022-07-29 08:30:20 -0300

    feat: create PasswordGeneratorBuilder using builder pattern

---
+++ src/br/com/ifpe/edu/recife/passwordgenerator/builder/PasswordGeneratorBuilder.java
@@ -0,0 +1,81 @@
+package br.com.ifpe.edu.recife.passwordgenerator.builder;
+
+import br.com.ifpe.edu.recife.passwordgenerator.generator.*;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PasswordGeneratorBuilder {
+    private PasswordGenerator passwordGenerator;
+    private int length;
+    private boolean withNumberGenerator;
+    private boolean withCharactersGenerator;
+    private boolean withSpecialCharactersGenerator;
+
+    public PasswordGeneratorBuilder() {
+        this.passwordGenerator = new BasePasswordGeneratorDecorator();
+        this.length = 5;
+    }
+
+    public PasswordGeneratorBuilder withLength(int length) {
+        this.length = returnLength(length);
+        return this;
+    }
+
+    public PasswordGeneratorBuilder withNumbersGenerator(boolean withNumberGenerator) {
+        this.withNumberGenerator = withNumberGenerator;
+        return this;
+    }
+
+    public PasswordGeneratorBuilder withCharactersGenerator(boolean withCharactersGenerator) {
+        this.withCharactersGenerator = withCharactersGenerator;
+        return this;
+    }
+
+    public PasswordGeneratorBuilder withSpecialCharactersGenerator(boolean withSpecialCharactersGenerator) {
+        this.withSpecialCharactersGenerator = withSpecialCharactersGenerator;
+        return this;
+    }
+
+    public String generatePassword() {
+        if (withNumberGenerator)
+            passwordGenerator = new NumbersDecorator(passwordGenerator);
+        if (withSpecialCharactersGenerator)
+            passwordGenerator = new SpecialCharactersDecorator(passwordGenerator);
+        if (withCharactersGenerator)
+            passwordGenerator = new CharactersDecorator(passwordGenerator);
+
+        String password = passwordGenerator.generatePassword(this.length);
+
+        this.passwordGenerator = new BasePasswordGeneratorDecorator();
+
+        if (password.length() > this.length) {
+            password = password.substring(0, this.length);
+        }
+
+        return shuffle(password);
+    }
+
+    private String shuffle(String password) {
+        List<String> letters = Arrays.stream(password.split("")).collect(Collectors.toList());
+        Collections.shuffle(letters);
+        return letters.stream().reduce("", (prev, next) -> prev + next);
+    }
+
+    private static int returnLength(int length) {
+        return Math.max(length, 5);
+    }
+
+    @Override
+    public String toString() {
+        return "PasswordGeneratorBuilder{" +
+                "passwordGenerator=" + passwordGenerator +
+                ", length=" + length +
+                ", withNumberGenerator=" + withNumberGenerator +
+                ", withCharactersGenerator=" + withCharactersGenerator +
+                ", withSpecialCharactersGenerator=" + withSpecialCharactersGenerator +
+                '}';
+    }
+}
